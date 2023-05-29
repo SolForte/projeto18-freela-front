8094929ac48c8b9d9ec73876e8b37489dfd594ea
SolForte <mateusbrandao1234@gmail.com>
2023-05-29 06:32:52 -0300

    build: pages and routes schematics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PassagensParaCidade from "./pages/PassagensParaCidade.js";
+import DetalhesPassagem from "./pages/DetalhesPassagem.js";
+import HospedagensEmCidade from "./pages/HospedagensEmCidade.js";
+import DetalhesDaHospedagem from "./pages/DetalheDaHospedagem.js";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cidades/:id/passagens" element={<PassagensParaCidade />} />
+        <Route path="/passagens/:id" element={<DetalhesPassagem />} />
+        <Route path="/cidades/:id/hospedagens" element={<HospedagensEmCidade />} />
+        <Route path="/hospedagens/:id" element={<DetalhesDaHospedagem />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
